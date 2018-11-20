63e51af584ad9ce5c9b5b28283b189087781ce8d
DEZAUTEZ Sébastien <DrCosinus@users.noreply.github.com>
2018-11-20 17:55:55 +0100

    card mvc (ts only)(not working yet)

--- MyBGWIP/js/mybgwip.ts
+++ MyBGWIP/js/mybgwip.ts
@@ -1,48 +1,15 @@
-class Card {
+// Model <- View : reads
+// Model <- Controller : updates
+// View -> Controller : writes/manipulates
+// Controller --> View : notifies/warns of changes (events)
+// User -> View : sees
+// User -> Controller : uses
+
+
+class CardModel {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-    }
-
-    static StaticInit(ctx: CanvasRenderingContext2D) {
-        var path = new Path2D();
-        path.arc(-Card.width_s/2 + Card.cornerRadius_s, -Card.height_s/2 + Card.cornerRadius_s, Card.cornerRadius_s, 2 * Math.PI / 2, 3 * Math.PI / 2);
-        path.arc(Card.width_s/2 - Card.cornerRadius_s, -Card.height_s/2 + Card.cornerRadius_s, Card.cornerRadius_s, 3 * Math.PI / 2, 0);
-        path.arc(Card.width_s/2 - Card.cornerRadius_s, Card.height_s/2 - Card.cornerRadius_s, Card.cornerRadius_s, 4 * Math.PI / 2, Math.PI / 2);
-        path.arc(-Card.width_s/2 + Card.cornerRadius_s, Card.height_s/2 - Card.cornerRadius_s, Card.cornerRadius_s, 1 * Math.PI / 2, 2 * Math.PI / 2);
-        path.closePath();
-        Card.path_s = path;
-
-        var gradient : CanvasGradient= ctx.createLinearGradient(0,0,Card.width_s,Card.height_s);
-        gradient.addColorStop(0,'rgba(250, 250, 250, 1)');
-        gradient.addColorStop(1,'rgba(120, 120, 180, 1)');
-        Card.gradient_s = gradient;
-    }
-
-    draw(ctx: CanvasRenderingContext2D, highlight:boolean) {
-        ctx.save();
-        this.applyTransform(ctx);
-        
-        ctx.fillStyle = Card.gradient_s;
-        ctx.fill(Card.path_s);
-
-        if (highlight)
-        {
-            ctx.fillStyle = 'rgba(0, 250, 0, 0.5)';
-            ctx.fill(Card.path_s);
-        }
-
-        ctx.strokeStyle = 'rgb(0,0,0)';
-        ctx.lineWidth = 2;
-        ctx.stroke(Card.path_s);
-
-        ctx.restore();
-    }
-
-    applyTransform(ctx: CanvasRenderingContext2D)
-    {
-        ctx.translate(this.x, this.y);
-        ctx.rotate(Math.PI*2*this.turn)
     }
 
     hittest(ctx: CanvasRenderingContext2D, x: number, y: number): boolean {
@@ -58,24 +25,75 @@ class Card {
         //this.x += 25;
         this.turn += 0.25;
     }
+    
+    x: number;
+    y: number;
+    turn: number = 0;
+};
 
+class CardView
+{
+    static StaticInit(ctx: CanvasRenderingContext2D) {
+        var path = new Path2D();
+        path.arc(-Card.width_s/2 + Card.cornerRadius_s, -Card.height_s/2 + Card.cornerRadius_s, Card.cornerRadius_s, 2 * Math.PI / 2, 3 * Math.PI / 2);
+        path.arc(Card.width_s/2 - Card.cornerRadius_s, -Card.height_s/2 + Card.cornerRadius_s, Card.cornerRadius_s, 3 * Math.PI / 2, 0);
+        path.arc(Card.width_s/2 - Card.cornerRadius_s, Card.height_s/2 - Card.cornerRadius_s, Card.cornerRadius_s, 4 * Math.PI / 2, Math.PI / 2);
+        path.arc(-Card.width_s/2 + Card.cornerRadius_s, Card.height_s/2 - Card.cornerRadius_s, Card.cornerRadius_s, 1 * Math.PI / 2, 2 * Math.PI / 2);
+        path.closePath();
+        Card.path_s = path;
+        
+        var gradient : CanvasGradient= ctx.createLinearGradient(0,0,Card.width_s,Card.height_s);
+        gradient.addColorStop(0,'rgba(250, 250, 250, 1)');
+        gradient.addColorStop(1,'rgba(120, 120, 180, 1)');
+        Card.gradient_s = gradient;
+    }
+    
+    draw(ctx: CanvasRenderingContext2D, highlight:boolean) {
+        ctx.save();
+        this.applyTransform(ctx);
+        
+        ctx.fillStyle = Card.gradient_s;
+        ctx.fill(Card.path_s);
+        
+        if (highlight)
+        {
+            ctx.fillStyle = 'rgba(0, 250, 0, 0.5)';
+            ctx.fill(Card.path_s);
+        }
+        
+        ctx.strokeStyle = 'rgb(0,0,0)';
+        ctx.lineWidth = 2;
+        ctx.stroke(Card.path_s);
+        
+        ctx.restore();
+    }
+
+    applyTransform(ctx: CanvasRenderingContext2D)
+    {
+        ctx.translate(this.x, this.y);
+        ctx.rotate(Math.PI*2*this.turn)
+    }
+    
     static cornerRadius_s: number = 10;
     static width_s: number = 88;
     static height_s: number = 63;
     static path_s: Path2D;
     static gradient_s: CanvasGradient;
-    x: number;
-    y: number;
-    turn: number = 0;
+    
+};
+
+class CardController
+{
+
 };
 
 // switch to Model-View-Controller : put draw (path,gradient,cornerRadius,...) out of "CardModel"
 
 class CardCollection {
     constructor() {
-
+        
     }
-
+    
     AddCard(x: number, y: number): Card {
         var card = new Card(x, y);
         this.cards.push(card);
